99dfc6a0c124c51662f640f09b40da8db1494b0c
rahltech <albertally265@gmail.com>
2026-02-21 21:28:04 +0300

    Make search function async and fetch results
    Refactor search function to be asynchronous and update results dynamically.

--- public/script.js
+++ public/script.js
@@ -1,8 +1,46 @@
-function search(){
+async function search(){
 
-let query = document.getElementById("search").value;
+let q = document.getElementById("search").value;
 
-document.getElementById("results").innerHTML =
-"Results for: " + query;
+let res = await fetch("/api/search?q="+q);
+
+let data = await res.json();
+
+let html = "";
+
+data.forEach(v => {
+
+html += `
+
+<p>
+
+${v.title}
+
+<br>
+
+<button onclick="play('${v.url}')">
+Play
+</button>
+
+<a href="/api/mp3?url=${v.url}">
+Download
+</a>
+
+</p>
+
+`;
+
+});
+
+document.getElementById("results").innerHTML = html;
+
+}
+
+
+
+function play(url){
+
+document.getElementById("player").src =
+"/api/stream?url="+url;
 
 }
