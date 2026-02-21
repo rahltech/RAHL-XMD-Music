86875e12a14ce0a1e84c01272ed09d94f0b8dd73
rahltech <albertally265@gmail.com>
2026-02-21 21:26:31 +0300

    Implement YouTube API search and streaming endpoints
    Added API endpoints for YouTube search and audio streaming.

--- server.js
+++ server.js
@@ -1,10 +1,61 @@
 const express = require("express");
-const path = require("path");
+const ytdl = require("ytdl-core");
+const ytSearch = require("yt-search");
 
 const app = express();
 
 app.use(express.static("public"));
 
-app.listen(3000, () => {
-console.log("RAHL XMD Music running");
+
+
+
+app.get("/api/search", async (req, res) => {
+
+const q = req.query.q;
+
+const result = await ytSearch(q);
+
+const videos = result.videos.slice(0, 10);
+
+res.json(videos);
+
 });
+
+
+
+
+
+app.get("/api/mp3", async (req, res) => {
+
+const url = req.query.url;
+
+res.header(
+"Content-Disposition",
+'attachment; filename="RAHL-XMD.mp3"'
+);
+
+ytdl(url, {
+filter: "audioonly"
+}).pipe(res);
+
+});
+
+
+
+
+
+app.get("/api/stream", async (req, res) => {
+
+const url = req.query.url;
+
+ytdl(url, {
+filter: "audioonly"
+}).pipe(res);
+
+});
+
+
+
+app.listen(3000, () =>
+console.log("RAHL XMD MUSIC RUNNING")
+);
