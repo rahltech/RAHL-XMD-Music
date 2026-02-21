501949a85a748a33280eff0242eccf4ef395e54b
rahltech <albertally265@gmail.com>
2026-02-21 21:40:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,16 +8,27 @@ app.use(express.static("public"));
 
 
 
-
 app.get("/api/search", async (req, res) => {
 
+try {
+
 const q = req.query.q;
+
+if (!q) return res.json([]);
 
 const result = await ytSearch(q);
 
 const videos = result.videos.slice(0, 10);
 
 res.json(videos);
+
+} catch (err) {
+
+console.log(err);
+
+res.status(500).send("Search error");
+
+}
 
 });
 
@@ -27,6 +38,8 @@ res.json(videos);
 
 app.get("/api/mp3", async (req, res) => {
 
+try {
+
 const url = req.query.url;
 
 res.header(
@@ -34,28 +47,52 @@ res.header(
 'attachment; filename="RAHL-XMD.mp3"'
 );
 
+res.header("Content-Type", "audio/mpeg");
+
 ytdl(url, {
 filter: "audioonly"
 }).pipe(res);
 
-});
+} catch {
 
+res.send("Download error");
+
+}
+
+});
 
 
 
 
 app.get("/api/stream", async (req, res) => {
 
+try {
+
 const url = req.query.url;
+
+res.header("Content-Type", "audio/mpeg");
 
 ytdl(url, {
 filter: "audioonly"
 }).pipe(res);
 
+} catch {
+
+res.send("Stream error");
+
+}
+
 });
 
 
 
-app.listen(3000, () =>
-console.log("RAHL XMD MUSIC RUNNING")
-);
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+
+console.log("RAHL XMD MUSIC RUNNING ON PORT " + PORT);
+
+});
